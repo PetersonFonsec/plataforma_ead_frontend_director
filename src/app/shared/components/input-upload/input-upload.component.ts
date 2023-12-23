4140626bf2909f0ee9ef107a7b61088660515682
peterson <petersonsimiao1@gmail.com>
2023-12-23 16:43:23 -0300

    feat: Criado componente input-upload

---
+++ src/app/shared/components/input-upload/input-upload.component.ts
@@ -0,0 +1,78 @@
+import { NgIf } from '@angular/common';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+export interface InputPhotoValue {
+  b64: string;
+  file: any;
+}
+
+@Component({
+  selector: 'input-upload',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './input-upload.component.html',
+  styleUrl: './input-upload.component.scss',
+  providers: [
+    {
+      multi: true,
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputUploadComponent),
+    },
+  ],
+})
+export class InputUploadComponent implements OnInit, ControlValueAccessor {
+  @Input() id = '';
+  @Input() initialImage?: string = '';
+
+  val: InputPhotoValue = {
+    b64: '',
+    file: '',
+  };
+
+  ngOnInit(): void { }
+
+  onChange: any = () => { };
+
+  onTouched: any = () => { };
+
+  get value() {
+    return this.val;
+  }
+
+  set value(val) {
+    if (val !== undefined && val !== null) {
+      this.val = val;
+      this.onChange(val);
+      this.onTouched(val);
+    }
+  }
+
+  onBlur() { }
+
+  writeValue(value: any) {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  showImage(event: any): void {
+    const files: FileList = event.target.files;
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.value = {
+        b64: reader.result as string,
+        file: files[0],
+      };
+    };
+
+    reader.readAsDataURL(files[0]);
+  }
+}
