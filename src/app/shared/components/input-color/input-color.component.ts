71acee739eebcbb99e3813152a17092d5c0bd377
peterson <petersonsimiao1@gmail.com>
2023-12-23 16:41:42 -0300

    feat: Criado compoenente input-color

---
+++ src/app/shared/components/input-color/input-color.component.ts
@@ -0,0 +1,57 @@
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormsModule } from '@angular/forms';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-input-color',
+  standalone: true,
+  imports: [FormsModule, NgStyle],
+  templateUrl: './input-color.component.html',
+  styleUrl: './input-color.component.scss',
+  providers: [
+    {
+      multi: true,
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputColorComponent),
+    },
+  ],
+})
+export class InputColorComponent implements OnInit, ControlValueAccessor {
+  @Input() error = false;
+  @Input() label = '';
+  @Input() id = '';
+  val = '';
+
+  ngOnInit(): void { }
+
+  onChange: any = () => { };
+
+  onTouched: any = () => { };
+
+  get value() {
+    return this.val;
+  }
+
+  set value(val) {
+    if (val !== undefined && val !== null) {
+      this.val = val;
+      this.onChange(val);
+      this.onTouched(val);
+    }
+  }
+
+  onBlur() { }
+
+  writeValue(value: any) {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+}
