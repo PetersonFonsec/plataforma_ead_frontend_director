20887dc385d441841a1af5e9790eb182b3869872
peterson <petersonsimiao1@gmail.com>
2024-03-30 10:45:17 -0300

    doc(button): Atualizado story

--- src/app/shared/components/button/button.stories.ts
+++ src/app/shared/components/button/button.stories.ts
@@ -1,50 +1,29 @@
-import { ButtonComponent } from './button.component';
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { ButtonColors, ButtonComponent } from './button.component';
+import { argsToTemplate } from '@storybook/angular';
+
+type ButtonStory = ButtonComponent & { label?: string };
 
 
-export default {
-  title: 'Button',
-  tags: ['autodocs'],
+const meta: Meta<ButtonStory> = {
   component: ButtonComponent,
-  template: `
-  <div style="padding: 3rem; max-width: 400px; width: 100%; margin: 0 auto">
-    <app-button>peterson</app-button>
-  </div>
-`,
+  render: ({ label, ...args }) => ({
+    props: args,
+    template: `
+    <app-button ${argsToTemplate(args)}>
+      ${label}
+    </app-button>`,
+  }),
+
 };
 
-export const Default = () => ({
-  component: ButtonComponent,
-  template: `
-  <div style="padding: 3rem; max-width: 400px; width: 100%; margin: 0 auto">
-    <app-button>peterson</app-button>
-  </div>
-`,
-})
+export default meta;
 
-export const Secundary = () => ({
-  component: ButtonComponent,
-  template: `
-  <div style="padding: 3rem; max-width: 400px; width: 100%; margin: 0 auto">
-  <app-button color="secundary" [disable]="false">peterson</app-button>
-  </div>
-`,
-})
+type Story = StoryObj<ButtonStory>;
 
-export const Small = () => ({
-  component: ButtonComponent,
-  template: `
-  <div style="padding: 3rem; max-width: 400px; width: 100%; margin: 0 auto">
-    <app-button color="primary" size="small" [disable]="false">peterson</app-button>
-  </div>
-`,
-})
-
-
-export const Disable = () => ({
-  component: ButtonComponent,
-  template: `
-  <div style="padding: 3rem; max-width: 400px; width: 100%; margin: 0 auto">
-    <app-button color="primary" [disabled]="true">peterson</app-button>
-  </div>
-`,
-})
+export const Primary: Story = {
+  args: {
+    label: "Peterson",
+  },
+};
