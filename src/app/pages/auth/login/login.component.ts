fc70996c4ed9ca427c51cb873943a52af9ce50f4
peterson <petersonsimiao1@gmail.com>
2024-04-14 22:07:49 -0300

    feat(login): Integrado com o back

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,24 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { InputTextComponent } from '../../../shared/components/input-text/input-text.component';
+import { AlertComponent, AlertTypes } from '../../../shared/components/alert/alert.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { ErrorDefault } from '../../../shared/interfaces/error.model';
+import { AuthService } from '../services/auth/auth.service';
+import { fadeAnimation } from '../../../shared/animations/fade/fade.animation';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonComponent, InputTextComponent, RouterLink, FormsModule],
+  imports: [ButtonComponent, InputTextComponent, RouterLink, FormsModule, AlertComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  animations: [fadeAnimation]
 })
 export class LoginComponent {
+  #authService = inject(AuthService);
+  #router = inject(Router);
+  alertType = AlertTypes.error;
+  error = ""
+
   payload = {
     password: "",
     email: "",
-  } as any;
+  };
 
   submit() {
-
+    this.#authService.login(this.payload).subscribe({
+      next: () => {
+        this.#router.navigateByUrl("/home")
+      },
+      error: (error: ErrorDefault) => {
+        this.error = error.message
+      }
+    })
   }
 }
---
+++ src/app/shared/interfaces/error.model.ts
@@ -0,0 +1,5 @@
+export interface ErrorDefault {
+  message: string,
+  error: string,
+  statusCode: number
+}
