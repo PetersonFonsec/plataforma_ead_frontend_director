ca5bb30a712ca2718a729236320ebb63324d943a
peterson <petersonsimiao1@gmail.com>
2024-04-15 13:14:56 -0300

    test(e2e auth):  Refatorado todos os testes para utilizar as novas classes e mock

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,122 +1,121 @@
+import LoginElements from "../support/pages/login";
+import RegisterElements from "../support/pages/register";
+import WellComeElements from "../support/pages/wellcome";
+
+const usuarios = require('../fixtures/usuarios.json')
+
 describe('Auth jorney - ', () => {
+  let loginElements: LoginElements;
+  let registerElements: RegisterElements;
+
+  beforeEach(() => {
+    loginElements = new LoginElements(usuarios);
+    registerElements = new RegisterElements(usuarios);
+  });
+
   describe('Test redirects', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:4200/')
+      cy.visit('http://localhost:53379/')
     });
 
     it("should redirect to /login page when click in 'login' button", () => {
-      cy.get("#welcome-login-button").click();
+      WellComeElements.loginButton.click();
       cy.url().should('include', '/entre/login');
     });
 
     it("should redirect to /cadastrar page when click in 'Cadastre-se' button", () => {
-      cy.get("#welcome-register-button").click();
+      WellComeElements.cadastroButton.click();
       cy.url().should('include', '/entre/cadastrar');
     });
 
     it("should redirect to /recuperar-senha page when click in 'Esqueceu a senha ? Clique aqui.' link", () => {
-      cy.visit('http://localhost:4200/entre/login');
-      cy.get("#login-forget-link").click();
+      cy.visit('http://localhost:53379/entre/login');
+      loginElements.forgotPasswordLink.click();
       cy.url().should('include', '/entre/recuperar-senha');
     });
 
     it("should redirect to /cadastrar page when click in 'Ainda não tem uma conta ? Cadastre-se !' button", () => {
-      cy.visit('http://localhost:4200/entre/login');
-      cy.get("#login-register-link").click();
+      cy.visit('http://localhost:53379/entre/login');
+      loginElements.registerLink.click();
       cy.url().should('include', '/entre/cadastrar');
     });
 
     it("should redirect to /login page when click in 'Já tem uma conta ? Entre!' button", () => {
-      cy.visit('http://localhost:4200/entre/cadastrar');
-      cy.get("#register-login-link").click();
+      cy.visit('http://localhost:53379/entre/cadastrar');
+      registerElements.loginLink.click();
       cy.url().should('include', '/entre/login');
     });
   });
 
   describe('login form validations ', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:4200/entre/login')
+      cy.visit('http://localhost:53379/entre/login');
+      loginElements.fillFormValid();
     });
 
     it('Should disabled submit button when form is empty', () => {
-      cy.get('#login-submit-button').should('be.disabled');
+      loginElements.clearForm();
+      loginElements.submitButton.should('be.disabled');
     });
 
     it('Should disabled submit button when email field is invalid', () => {
-      cy.get('#email').type('emailInvalid');
-      cy.get('#password').type('password');
-      cy.get('#login-submit-button').should('be.disabled');
+      loginElements.emailInput.clear().type('emailInvalid');
+      loginElements.submitButton.should('be.disabled');
     });
 
     it('Should disabled submit button when email field is empty', () => {
-      cy.get('#password').type('password');
-      cy.get('#login-submit-button').should('be.disabled');
+      loginElements.emailInput.clear();
+      loginElements.submitButton.should('be.disabled');
     });
 
     it('Should disabled submit button when password field is empty', () => {
-      cy.get('#email').type('emailInvalid');
-      cy.get('#login-submit-button').should('be.disabled');
+      loginElements.passwordInput.clear();
+      loginElements.submitButton.should('be.disabled');
     });
 
     it('Should enable submit button when form is valid', () => {
-      cy.get('#email').type('email@valid.com');
-      cy.get('#password').type('password');
-      cy.get('#login-submit-button').should('be.enabled');
+      loginElements.submitButton.should('be.enabled');
     });
   });
 
-  describe('register form', () => {
+  describe('register form validations', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:4200/entre/cadastrar');
+      cy.visit('http://localhost:53379/entre/cadastrar');
+      registerElements.fillFormValid();
     });
 
     it('Should disabled submit button when form is empty', () => {
-      cy.get('#register-submit-button').should('be.disabled');
+      registerElements.clearFormValid();
+      registerElements.submitButton.should('be.disabled');
     });
 
     it('Should disabled submit button when email field is invalid', () => {
-      cy.get('#email').type('emailInvalid');
-      cy.get('#password').type('password');
-      cy.get('#confirm_password').type('password');
-      cy.get('#document_number').type('14190806013');
-      cy.get('#register-submit-button').should('be.disabled');
+      registerElements.emailInput.clear().type('emailInvalid');
+      registerElements.submitButton.should('be.disabled');
     });
 
     it('Should disabled submit button when email field is empty', () => {
-      cy.get('#password').type('password');
-      cy.get('#confirm_password').type('password');
-      cy.get('#document_number').type('14190806013');
-      cy.get('#register-submit-button').should('be.disabled');
+      registerElements.emailInput.clear();
+      registerElements.submitButton.should('be.disabled');
     });
 
     it('Should disabled submit button when password field is empty', () => {
-      cy.get('#email').type('emailInvalid');
-      cy.get('#confirm_password').type('password');
-      cy.get('#document_number').type('14190806013');
-      cy.get('#register-submit-button').should('be.disabled');
+      registerElements.passwordInput.clear();
+      registerElements.submitButton.should('be.disabled');
     });
 
     it('Should disabled submit button when "confirm_password" field is empty', () => {
-      cy.get('#email').type('email@valid.com');
-      cy.get('#password').type('password');
-      cy.get('#document_number').type('14190806013');
-      cy.get('#register-submit-button').should('be.disabled');
+      registerElements.confirmPasswordInput.clear();
+      registerElements.submitButton.should('be.disabled');
     });
 
     it('Should disabled submit button when "document_number" field is empty', () => {
-      cy.get('#email').type('email@valid.com');
-      cy.get('#password').type('password');
-      cy.get('#confirm_password').type('password');
-      cy.get('#register-submit-button').should('be.disabled');
+      registerElements.documentNumberInput.clear();
+      registerElements.submitButton.should('be.disabled');
     });
 
     it('Should enable submit button when form is valid', () => {
-      cy.get('#email').type('email@valid.com');
-      cy.get('#password').type('password');
-      cy.get('#confirm_password').type('password');
-      cy.get('#document_number').type('14190806013');
-      cy.get('#register-submit-button').should('be.enabled');
+      registerElements.submitButton.should('be.enabled');
     });
-
   });
 });
