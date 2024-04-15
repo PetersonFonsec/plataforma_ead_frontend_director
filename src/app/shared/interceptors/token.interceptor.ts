afac4c21d23b44f0a78f10eb7cee4a342e8fe235
peterson <petersonsimiao1@gmail.com>
2024-04-14 22:02:48 -0300

    feat: Criado interceptor token

---
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from "@angular/core";
+
+import { TokenService } from '../services/token/token.service';
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const tokenService = inject(TokenService);
+
+  if (tokenService.token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${}`,
+        Accept: 'application/json',
+      },
+    });
+  }
+
+  return next(req);
+};
