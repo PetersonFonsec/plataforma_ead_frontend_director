b46954cc67049dd5e359ea78b2a7223c9593831a
peterson <petersonsimiao1@gmail.com>
2024-04-15 10:43:58 -0300

    feat(register): Adicionado  mascara no campo CPF

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -6,17 +6,18 @@ import { Router, RouterLink } from '@angular/router';
 import { InputTextComponent } from '../../../shared/components/input-text/input-text.component';
 import { AlertComponent, AlertTypes } from '../../../shared/components/alert/alert.component';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
-import { ErrorDefault } from '../../../shared/interfaces/error.model';
 import { AuthService } from '../services/auth/auth.service';
 import { Roles } from '../../../shared/enums/roles.enum';
 import { HttpErrorResponse } from '@angular/common/http';
+import { fadeAnimation } from '../../../shared/animations/fade/fade.animation';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [ButtonComponent, InputTextComponent, RouterLink, FormsModule, AlertComponent],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
+  animations: [fadeAnimation]
 })
 export class RegisterComponent {
   #authService = inject(AuthService);
