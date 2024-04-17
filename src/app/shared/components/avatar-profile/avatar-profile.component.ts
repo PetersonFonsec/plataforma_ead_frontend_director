b3cbbc2b4ce1f786da2714dedbc1901092bee2ea
peterson <petersonsimiao1@gmail.com>
2024-04-17 16:07:49 -0300

    feat(shared component): Criado componente avatar-profile

---
+++ src/app/shared/components/avatar-profile/avatar-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AvatarComponent } from '../avatar/avatar.component';
+
+@Component({
+  selector: 'app-avatar-profile',
+  standalone: true,
+  imports: [AvatarComponent],
+  templateUrl: './avatar-profile.component.html',
+  styleUrl: './avatar-profile.component.scss'
+})
+export class AvatarProfileComponent {
+
+}
