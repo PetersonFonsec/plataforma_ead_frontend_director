21a59f05d0e96d5414e1e5af11df2f450a584e99
peterson <petersonsimiao1@gmail.com>
2024-06-27 07:44:17 -0300

    config: Configurado quill lib para componente de textp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,7 @@ import { routes } from './app.routes';
 import { tokenInterceptor } from '@shared/interceptors/token/token.interceptor';
 import { errorInterceptor } from '@shared/interceptors/error/error.interceptor';
 import { editorConfig } from '@shared/components/text-editor/text-editor.config';
+import Counter from '@shared/components/text-editor/counter';
 
 const maskConfig: Partial<IConfig> = {
   validation: false,
@@ -21,6 +22,17 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideHttpClient(withFetch(), withInterceptors([tokenInterceptor, errorInterceptor])),
     provideEnvironmentNgxMask(maskConfig),
-    provideQuillConfig(editorConfig)
+    provideQuillConfig(
+      {
+        customModules: [{
+          implementation: Counter,
+          path: 'modules/counter'
+        }],
+        customOptions: [{
+          import: 'formats/font',
+          whitelist: ['mirza', 'roboto', 'aref', 'serif', 'sansserif', 'monospace']
+        }]
+      }
+    )
   ]
 };
--- src/app/pages/logged-area/pages/lesson/lesson.component.ts
+++ src/app/pages/logged-area/pages/lesson/lesson.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser'
+import { QuillEditorComponent } from 'ngx-quill'
 
 @Component({
   selector: 'app-lesson',
   standalone: true,
-  imports: [],
+  imports: [QuillEditorComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './lesson.component.html',
-  styleUrl: './lesson.component.scss'
+  styleUrl: './lesson.component.scss',
 })
 export class LessonComponent {
+  textFormat = ''
+  form: FormGroup = this.fb.group({
+    html: new FormControl('<div>test</div><ul><li>1</li><li class="ql-indent-1">1-1</li><li>2</li><ol><li>numbered</li><li class="ql-indent-1">numbered-1</li></ol></ul><div><br></div>'),
+    matQuillHtml: new FormControl('<div>test</div><ul><li>1</li><li class="ql-indent-1">1-1</li><li>2</li><ol><li>numbered</li><li class="ql-indent-1">numbered-1</li></ol></ul><div><br></div>')
+  })
 
+  constructor(private sanitizer: DomSanitizer, private fb: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  byPassHTML(html: string) {
+    return this.sanitizer.bypassSecurityTrustHtml(html)
+  }
 }
