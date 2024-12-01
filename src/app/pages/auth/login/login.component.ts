5b266352d9686be0eb529d1d2707cca20736cc92
peterson <petersonsimiao1@gmail.com>
2024-12-01 12:19:26 -0300

    feat(login page): adicionado liveAnnouncer

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 import { InputTextComponent } from '../../../shared/components/input-text/input-text.component';
 import { AlertComponent, AlertTypes } from '../../../shared/components/alert/alert.component';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
-import { ErrorDefault } from '../../../shared/interfaces/error.model';
 import { AuthService } from '../services/auth/auth.service';
 import { fadeAnimation } from '../../../shared/animations/fade/fade.animation';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -19,6 +19,7 @@ import { HttpErrorResponse } from '@angular/common/http';
   animations: [fadeAnimation]
 })
 export class LoginComponent {
+  #liveAnnouncer = inject(LiveAnnouncer);
   #authService = inject(AuthService);
   alertType = AlertTypes.error;
   #router = inject(Router);
@@ -32,9 +33,11 @@ export class LoginComponent {
   submit() {
     this.#authService.login(this.payload).subscribe({
       next: () => {
+        this.#liveAnnouncer.announce("Login realizado com sucesso");
         this.#router.navigateByUrl("/area-logada/home");
       },
       error: (error: HttpErrorResponse) => {
+        this.#liveAnnouncer.announce("Houve um erro ao realizar login");
         this.error = error.error.message;
       }
     })
